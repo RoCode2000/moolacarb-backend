8b954fcb3b0aab535bd7a5e47e9933164dcc5a59
Roger <rtgx2000@gmail.com>
2025-10-03 21:56:30 +0800

    updated for cloud use

--- src/main/java/com/uow/moolacarb/MoolacarbApplication.java
+++ src/main/java/com/uow/moolacarb/MoolacarbApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoolacarbApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MoolacarbApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoolacarbApplication.class, args);
+    }
 }
