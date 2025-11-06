28e7eed43edbb5e15beee996b376c967d251d5c0
Roger <rtgx2000@gmail.com>
2025-11-06 10:38:53 +0800

    added ip for communication with server

--- src/main/java/com/uow/moolacarb/WebConfig.java
+++ src/main/java/com/uow/moolacarb/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // React fronend dev server
+            .allowedOrigins("http://localhost:3000", "http://4.190.64.83:3000") // React fronend dev server
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
     }
